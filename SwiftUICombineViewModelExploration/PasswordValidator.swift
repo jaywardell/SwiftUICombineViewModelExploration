761a979736eeccb02f51b281cca9026203c2fb40
Joseph Wardell <jaywardell@mac.com>
2020-12-24 10:26:12 -0500

    refactored PasswordValidator to use an internal validation method to returns an enum and determine then password failure reason from that enum

--- SwiftUICombineViewModelExploration/PasswordValidator.swift
+++ SwiftUICombineViewModelExploration/PasswordValidator.swift
@@ -12,6 +12,37 @@ public struct PasswordValidator: CredentialsValidator {
     public init() {}
     
     public func validate<C, V>(_ credentials: C) -> V where C : Credentials, V : CredentialsValidation {
-        .init(passwordFeedback: "", isValid: false)
+
+        let validation: Validation = validate(credentials)
+        return .init(passwordFeedback: validation.reason, isValid: validation == .valid)
+    }
+    
+    public enum Validation: Equatable {
+        case valid
+        case emptyUsername
+        case emptyPassword
+        case passwordsDoNotMatch
+        
+        var reason: String {
+            switch self {
+            case .valid: return ""
+            case .emptyUsername: return "Username cannot be empty"
+            case .emptyPassword: return "Password cannot be emoty"
+            case .passwordsDoNotMatch: return "Passwords do not match"
+            }
+        }
+    }
+    
+    public func validate<C>(_ credentials: C) -> Validation where C : Credentials {
+        if credentials.username.isEmpty {
+            return .emptyUsername
+        }
+        else if credentials.password.isEmpty {
+            return .emptyPassword
+        }
+        else {
+            return .passwordsDoNotMatch
+        }
+//        return .emptyUsername
     }
 }
--- SwiftUICombineViewModelExplorationUITests/PasswordValidatorTests.swift
+++ SwiftUICombineViewModelExplorationUITests/PasswordValidatorTests.swift
@@ -26,28 +26,28 @@ class PasswordValidatorTests: XCTestCase {
         let sut = PasswordValidator()
         let emptyCredentials = MockCredentials(username: "", password: "", passwordAgain: "")
        
-        expect(false, from: sut, for: emptyCredentials)
+        expect(.emptyUsername, from: sut, for: emptyCredentials)
     }
     
     func test_validate_emptyPasswordIsInvalid() {
         let sut = PasswordValidator()
-        let emptyPasswordCredentials = MockCredentials(username: "", password: "", passwordAgain: "")
-        
-        expect(false, from: sut, for: emptyPasswordCredentials)
+        let emptyPasswordCredentials = MockCredentials(username: "George", password: "", passwordAgain: "")
+
+        expect(.emptyPassword, from: sut, for: emptyPasswordCredentials)
     }
 
     func test_validate_emptyNonMatchingPasswordfsAreInvalid() {
         let sut = PasswordValidator()
-        let nonmatchingPasswords = MockCredentials(username: "", password: "hi", passwordAgain: "bye")
-        
-        expect(false, from: sut, for: nonmatchingPasswords)
+        let nonmatchingPasswords = MockCredentials(username: "George", password: "hi", passwordAgain: "bye")
+
+        expect(.passwordsDoNotMatch, from: sut, for: nonmatchingPasswords)
     }
 
     // Mark:- Helpers
     
-    func expect<C: Credentials>(_ valid: Bool, from sut: PasswordValidator, for input: C, file: StaticString = #filePath, line: UInt = #line) {
-        let validation: MockValidation = sut.validate(input)
+    func expect<C: Credentials>(_ expected: PasswordValidator.Validation, from sut: PasswordValidator, for input: C, file: StaticString = #filePath, line: UInt = #line) {
+        let validation: PasswordValidator.Validation = sut.validate(input)
 
-        XCTAssertEqual(validation.isValid, false, file: file, line: line)
+        XCTAssertEqual(validation, expected, "\nexpected: \(expected)\ngot: \(validation)", file: file, line: line)
     }
 }
