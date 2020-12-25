9f077a24229a3c90c67c26fb638f24acf6abed4e
Joseph Wardell <jaywardell@mac.com>
2020-12-25 09:21:47 -0500

    PasswordValidator now uses Error internally, rather than the custom Validation type it was using

--- SwiftUICombineViewModelExploration/PasswordValidator.swift
+++ SwiftUICombineViewModelExploration/PasswordValidator.swift
@@ -42,56 +42,56 @@ public struct PasswordValidator {
         }
     }
 
-    public enum Validation: Equatable {
-        case valid
-        case emptyUsername
-        case usernameIsTooShort
-        case emptyPassword
-        case passwordsIsTooShort
-        case emptyPasswordVerification
-        case passwordsDoNotMatch
-        case passwordNeedsANumber
-        case passwordNeedsALowercaseLetter
-        case passwordNeedsAnUppercaseLetter
-
-        public static let MinUsernameLength: Int = 3
-        public static let MinPasswordLength: Int = 5
-
-        var explanantion: String {
-            switch self {
-            case .valid: return ""
-            case .emptyUsername: return "Username cannot be empty"
-            case .usernameIsTooShort: return "Username must be at least \(Self.MinUsernameLength) characters long"
-                
-            // if the user hasn't yet typed the password or password verification, don't report anything in the UI
-            case .emptyPassword: return ""
-            case .emptyPasswordVerification: return ""
-                
-            case .passwordsIsTooShort: return "Passwords must be at least \(Self.MinPasswordLength) characters long"
-            case .passwordsDoNotMatch: return "Passwords do not match"
-            case .passwordNeedsANumber: return "Passwords need at least one number"
-            case .passwordNeedsALowercaseLetter: return "Passwords need at least one lowercase letter"
-            case .passwordNeedsAnUppercaseLetter: return "Passwords need at least one uppercase letter"
-            }
-        }
-    }
+//    public enum Validation: Equatable {
+//        case valid
+//        case emptyUsername
+//        case usernameIsTooShort
+//        case emptyPassword
+//        case passwordsIsTooShort
+//        case emptyPasswordVerification
+//        case passwordsDoNotMatch
+//        case passwordNeedsANumber
+//        case passwordNeedsALowercaseLetter
+//        case passwordNeedsAnUppercaseLetter
+//
+//        public static let MinUsernameLength: Int = 3
+//        public static let MinPasswordLength: Int = 5
+//
+//        var explanantion: String {
+//            switch self {
+//            case .valid: return ""
+//            case .emptyUsername: return "Username cannot be empty"
+//            case .usernameIsTooShort: return "Username must be at least \(Self.MinUsernameLength) characters long"
+//                
+//            // if the user hasn't yet typed the password or password verification, don't report anything in the UI
+//            case .emptyPassword: return ""
+//            case .emptyPasswordVerification: return ""
+//                
+//            case .passwordsIsTooShort: return "Passwords must be at least \(Self.MinPasswordLength) characters long"
+//            case .passwordsDoNotMatch: return "Passwords do not match"
+//            case .passwordNeedsANumber: return "Passwords need at least one number"
+//            case .passwordNeedsALowercaseLetter: return "Passwords need at least one lowercase letter"
+//            case .passwordNeedsAnUppercaseLetter: return "Passwords need at least one uppercase letter"
+//            }
+//        }
+//    }
     
     static let HasNumberPredicate: NSPredicate = NSPredicate(format: "SELF MATCHES %@", ".*[0-9].*")
     static let HasLowercaseLetterPredicate: NSPredicate = NSPredicate(format: "SELF MATCHES %@", ".*[a-z].*")
     static let HasUppercaseLetterPredicate: NSPredicate = NSPredicate(format: "SELF MATCHES %@", ".*[A-Z].*")
 
-    private func val<C>(_ credentials: C) -> Validation where C : Credentials {
-        guard !credentials.username.isEmpty else { return .emptyUsername }
-        guard credentials.username.count >= Validation.MinUsernameLength else { return .usernameIsTooShort }
-        guard !credentials.password.isEmpty else { return .emptyPassword }
-        guard Self.HasNumberPredicate.evaluate(with: credentials.password) else { return .passwordNeedsANumber }
-        guard Self.HasLowercaseLetterPredicate.evaluate(with: credentials.password) else { return .passwordNeedsALowercaseLetter }
-        guard Self.HasUppercaseLetterPredicate.evaluate(with: credentials.password) else { return .passwordNeedsAnUppercaseLetter }
-        guard credentials.password.count >= Validation.MinPasswordLength else { return .passwordsIsTooShort }
-        guard !credentials.passwordAgain.isEmpty else { return .emptyPasswordVerification }
-        guard credentials.password == credentials.passwordAgain else { return .passwordsDoNotMatch }
+    private func val<C>(_ credentials: C) -> Error? where C : Credentials {
+        guard !credentials.username.isEmpty else { return ValidationError(Self.EmptyUsername) }
+        guard credentials.username.count >= Validation.MinUsernameLength else { return ValidationError(Self.UserNameTooShort) }
+        guard !credentials.password.isEmpty else { return ValidationError(Self.EmptyPassword) }
+        guard Self.HasNumberPredicate.evaluate(with: credentials.password) else { return ValidationError(Self.PasswordsLackANumber) }
+        guard Self.HasLowercaseLetterPredicate.evaluate(with: credentials.password) else { return ValidationError(Self.PasswordsLackALowercaseLetter) }
+        guard Self.HasUppercaseLetterPredicate.evaluate(with: credentials.password) else { return ValidationError(Self.PasswordsLackAnUppercaseLetter) }
+        guard credentials.password.count >= Validation.MinPasswordLength else { return ValidationError(Self.PasswordTooShort) }
+        guard !credentials.passwordAgain.isEmpty else { return ValidationError(Self.EmptyPassword) }
+        guard credentials.password == credentials.passwordAgain else { return ValidationError(Self.PasswordsDoNotMatch) }
         
-        return .valid
+        return nil
     }
 }
 
@@ -106,12 +106,13 @@ struct PasswordRequirements: CredentialsValidator {
 extension PasswordValidator: CredentialsValidator {
             
     public func validate<C: Credentials>(_ credentials: C, completion: @escaping (Error?)->()) {
-        let validation: Validation = val(credentials)
-        switch validation {
-        case .valid:
-            completion(nil)
-        default:
-            completion(ValidationError(validation.explanantion))
-        }
+        let error = val(credentials)
+        completion(error)
+//        switch validation {
+//        case .valid:
+//            completion(nil)
+//        default:
+//            completion(ValidationError(validation.explanantion))
+//        }
     }
 }
